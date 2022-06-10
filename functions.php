1b2094cc0172692653b1ec2f0902031b0ff55216
Srdjan Mitrovic <iam@mitrovics.com>
2022-06-10 14:14:44 +0200

    forms zips

--- functions.php
+++ functions.php
@@ -30,6 +30,7 @@ $understrap_includes = array(
 	'/settings.php',                        // Theme Settings
 	'/customize.php',                       // Customize theme
 	'/ctp.php',                             // Custom Post Types
+	'/forms.php',                           // Quote Forms
 );
 
 foreach ( $understrap_includes as $file ) {
@@ -256,41 +257,6 @@ function searchfilter($query) {
 return $query;
 }
 add_filter('pre_get_posts','searchfilter');
-
-
-
-
-function cf7_post_to_third_party($form)
-{
-    $formMappings = array(
-        'first_name' => array('your-first','your-name'),
-		'last_name' => array('your-name'),
-		'email' => array('your-email'),
-		'phone' => array('your-tel','telephone'),
-		'move_date' => array('your-date','movement-date'),
-		'move_size' => array('your-size','movement-choice'),
-		'from_zip' => array('zip-from'),
-		'to_zip' => array('to-zip'),
-		'car_trailer' => array('trailer-type','trailsize'),
-		'car_make' => array('car-make'),
-		'car_model' => array('car-model'),
-		'car_year' => array('car-year'),
-        'source_details[url]' => array('dynamichidden-672'),
-        'source_details[title]' => array('dynamichidden-673')
-    );
-    $handler = new MovingSoftFormHandler($formMappings);
-    $handler->setOrigin('https://longdistanceusamovers.com')->handleCF7($form, [135965, 135964, 135966]);
-}
-add_action('wpcf7_mail_sent', 'cf7_post_to_third_party', 10, 1);
-
-
-function skip_mail_when_testing($f){
-    $submission = WPCF7_Submission::get_instance();
-    $handler = new MovingSoftFormHandler();
-    
-    return $handler->getIP() == '206.189.212.83'; //testing Bot IP address
-}
-add_filter('wpcf7_skip_mail','skip_mail_when_testing');
 
 add_filter( 'wpcf7_autop_or_not', '__return_false' );
 
